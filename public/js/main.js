8d006d7d8b65ddf72fabc580e1d4ecd0ad89b3b3
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:12:31 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,119 @@
+// let num = "1";
+
+// exo1
+
+// if(1 == "1"){
+//     console.log("vrai");
+// }
+// else{
+//     console.log("faux");
+// }
+// console.log();
+
+// exo2
+
+// if(1 === "1"){
+//     console.log("vrai");
+// }
+// else{
+//     console.log("faux");
+// }
+// console.log();
+
+// exo3
+// let prenom = prompt("prenom?");
+// let taille = prenom.length;
+// if (taille < 5) {
+//     alert("prenom trop court");
+// }
+// else{
+//     alert("parfait");
+// }
+
+// exo4
+
+// let addition = parseInt(prompt("5+5"));
+
+// if (addition === 10) {
+//     alert("bravo")
+// } else {
+//     alert("dommage reéssaye")
+// }
+
+// exo5
+
+// let multi = prompt("3*3")
+
+// if (multi==9) {
+//     alert(`la reponse était bien ${multi}`)
+// } else {
+//     alert(`C'est dommage, tu n'étais qu'a ${Math.abs(multi - 9)} unité de la bonne réponse `  )
+// }
+
+// exo6
+
+// let tab = []
+// let question;
+// if (tab.length == 3) {
+//     alert(tab)
+// } else {
+//     question = prompt("entre un element")
+//     tab.push(question);
+// }
+// if (tab.length == 3) {
+//     alert(tab)
+// } else {
+//     question = prompt("entre un element")
+//     tab.push(question)
+// }
+// if (tab.length == 3) {
+//     alert(tab)
+// } else {
+//     question = prompt("entre un element")
+//     tab.push(question)
+// }
+// if (tab.length == 3) {
+//     alert(tab)
+// } else {
+//     question = prompt("entre un element")
+//     tab.push(question)
+// }
+// alert(tab);
+
+// exo7
+
+// let n1 = prompt("entrez un nombre")
+// let n2 = prompt("entrez un nombre")
+// let n3 = prompt("entrez un nombre")
+// let grandNombres =[];
+// let petitNombres =[];
+// if (n1 >= 12) {
+//     grandNombres.push(n1)
+// } else {
+    
+// }
+// if (n2 >= 12) {
+//     grandNombres.push(n1)
+// } else {
+    
+// }
+// if (n3 >= 12) {
+//     grandNombres.push(n1)
+// } else {
+    
+// }
+// alert(n1 + n2 + n3 )
+
+// exo8
+
+
+// let jourSemaine = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi'];
+// let question = prompt("Jour de la semaine?").toLowerCase();
+
+// if (question == jourSemaine[jourSemaine.indexOf('lundi')] || question == jourSemaine[jourSemaine.indexOf('mardi')] || question == jourSemaine[jourSemaine.indexOf('mercredi')]  ) {
+//     console.log('Vous êtes en e-learning');
+// } else if (question == jourSemaine[jourSemaine.indexOf('jeudi')] || question == jourSemaine[jourSemaine.indexOf('vendredi')]) {
+//     console.log('on est à MG');
+// } else {
+//     console.log(`Cette option n'est pas valable`);
+// }
